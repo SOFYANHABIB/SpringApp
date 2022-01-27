ea678fd09ee3d0e4f9d934f63401baf069c7f009
MEHDIHABIB <78062259+MEHDIHABIB@users.noreply.github.com>
2022-01-27 23:33:59 +0000

    test controller complete

--- src/main/java/com/qa/chocolate/TestController.java
+++ src/main/java/com/qa/chocolate/TestController.java
@@ -1,5 +1,20 @@
 package com.qa.chocolate;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
 public class TestController {
+	
+	@GetMapping("/test")
+	public String hello() {
+		
+		
+		return "Hello World!";
+	}
+	
+	
 
 }
